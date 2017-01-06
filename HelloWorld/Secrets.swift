c6c554f4c59513411e27340175b38b2fc10ddc1e
github username <cenkerdemir@yahoo.com>
2017-01-06 16:20:14 -0500

    new file added for the API key

---
+++ HelloWorld/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  HelloWorld
+//
+//  Created by Cenker Demir on 1/6/17.
+//  Copyright © 2017 Cenker Demir. All rights reserved.
+//
+
+import Foundation
+
+public let forecastKey = "blah blah blah"
+
